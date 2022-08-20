9b0dfd36de556fa2e5fe7d0f3d6e9e49f8a0757c
kate <parkjisu6239@gmail.com>
2022-08-20 17:56:37 +0900

    main size fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,32 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-
+import { css } from "@emotion/css";
 import Header from "src/components/organisms/Header";
-
 import "./App.css";
 
+const mainCss = (FixedHeight: number) => css`
+  height: 100%;
+  min-height: calc(100vh - ${FixedHeight}px);
+`;
+
 function App() {
+  const [fixedHeight, setFixedHeight] = React.useState(0);
+
+  React.useEffect(() => {
+    const header = document.querySelector("header");
+    const footer = document.querySelector("footer");
+    const headerHeight = header ? header.clientHeight : 0;
+    const footerHeight = footer ? footer.clientHeight : 0;
+
+    setFixedHeight(headerHeight + footerHeight);
+  }, []);
+
   return (
     <div className="App">
       <Header />
-      <Outlet />
+      <main className={mainCss(fixedHeight)}>
+        <Outlet />
+      </main>
       <footer>Footer</footer>
     </div>
   );
